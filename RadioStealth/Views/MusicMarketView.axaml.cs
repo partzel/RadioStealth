d2fa07e10aec1e9b4ab9b3fdfe7519859a9840d5
partzel <ahmedelmahdibendou2003@gmail.com>
2025-04-09 16:46:57 +0100

    feat: add Music Market View

---
+++ RadioStealth/Views/MusicMarketView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RadioStealth;
+
+public partial class MusicMarketView : UserControl
+{
+    public MusicMarketView()
+    {
+        InitializeComponent();
+    }
+}
